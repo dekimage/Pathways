a6fe4c936533be9b305c2eeedd01d54780a34062
dekimage <dejan.gavrilovikk@gmail.com>
2024-05-17 00:35:13 +0200

    hide landing

--- src/app/page.js
+++ src/app/page.js
@@ -191,6 +191,7 @@ const PricingBox = ({ pricingData }) => {
 };
 
 const LandingPage = () => {
+  return <div>Landing page in progress</div>;
   return (
     <div>
       <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 lg:items-start px-8 py-8 lg:py-20">
